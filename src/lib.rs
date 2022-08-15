ab7b58ebf5bb5733506d9908e804ba33f695510b
edwar4rd <edwar4rd@gmail.com>
2022-08-16 01:42:05 +0800

    Move SVG output logic to lib.rs, add output all
    Move SVG logic to lib.rs:
        svg_empty_document return a file with black background rect
        svg_output_to_document read a point vector and write to ducument
            output all path, non-zero path and points
              with different saturation (40%, 70%, 100%)
              and different opacity (0.3, 0.6, 0.8)
            consume document, return document
    
    Output All:
        Dump all given entry to a single svg (not ya best idea)
        (this_hue = last_hue + 7) mod 360
        Resulting file might (very likely) be unusable

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::fs;
 use std::path::Path;
+use svg::node::element::path::Data as SVGData;
+use svg::node::element::Path as SVGPath;
+use svg::node::element::Rectangle as SVGRectangle;
+use svg::Document as SVGDocument;
 
 pub struct Config {
     pub filenames: Vec<String>,
@@ -142,4 +146,115 @@ impl PtflParser {
         }
         Ok(file_entry_num)
     }
+}
+
+pub fn svg_empty_document(scale: f64, clip_pos: f64) -> SVGDocument {
+    svg::Document::new()
+        .set("width", format!("{}px", (scale * clip_pos * 2.0) as u32))
+        .set("height", format!("{}px", (scale * clip_pos * 2.0) as u32))
+        .set(
+            "viewBox",
+            (0, 0, scale * clip_pos * 2.0, scale * clip_pos * 2.0),
+        )
+        .add(
+            SVGRectangle::new()
+                .set("fill", "black")
+                .set("width", "100%")
+                .set("height", "100%"),
+        )
+}
+
+pub fn svg_output_to_document(
+    document: SVGDocument,
+    points: &Vec<(f64, f64)>,
+    clip_pos: f64,
+    scale: f64,
+    hue: f64,
+    brightness: u32,
+) -> SVGDocument {
+    let all_path = SVGPath::new()
+        .set("fill", "none")
+        .set("stroke", format!("hsla({hue},40%,{brightness}%, 0.3)"))
+        .set("stroke-width", scale * 0.0005)
+        .set("d", all_path_svgdata(&points, clip_pos, scale));
+
+    let nz_path = SVGPath::new()
+        .set("fill", "none")
+        .set("stroke", format!("hsla({hue},70%,{brightness}%, 0.6)"))
+        .set("stroke-width", scale * 0.003)
+        .set("d", non_zero_path_svgdata(&points, clip_pos, scale));
+
+    let points_path = SVGPath::new()
+        .set("fill", "none")
+        .set("stroke", format!("hsla({hue},100%,{brightness}%, 0.8)"))
+        .set("stroke-width", scale * 0.002)
+        .set(
+            "d",
+            non_zero_path_square_svgdata(&points, clip_pos, scale, 0.01),
+        );
+
+    document.add(all_path).add(nz_path).add(points_path)
+}
+
+fn all_path_svgdata(points: &Vec<(f64, f64)>, clip_pos: f64, scale: f64) -> SVGData {
+    let mut data = SVGData::new().move_to((
+        scale * (&points[0].1 * &points[0].0.cos() + clip_pos),
+        scale * (&points[0].1 * &points[0].0.sin() + clip_pos),
+    ));
+    for j in points {
+        data = data.line_to((
+            scale * (j.1 * j.0.cos() + clip_pos),
+            scale * (j.1 * j.0.sin() + clip_pos),
+        ));
+    }
+    data.close()
+}
+
+fn non_zero_path_svgdata(points: &Vec<(f64, f64)>, clip_pos: f64, scale: f64) -> SVGData {
+    let mut data = SVGData::new().move_to({
+        let mut a = points.iter();
+        loop {
+            let j = match a.next() {
+                Some(next) => next,
+                None => break (0.0, 0.0),
+            };
+            if j.1 != 0.0 {
+                break (
+                    scale * (j.1 * (j.0.cos()) + clip_pos),
+                    scale * (j.1 * (j.0.sin()) + clip_pos),
+                );
+            }
+        }
+    });
+    for j in points {
+        if j.1 != 0.0 {
+            data = data.line_to((
+                scale * (j.1 * (j.0.cos()) + clip_pos),
+                scale * (j.1 * (j.0.sin()) + clip_pos),
+            ));
+        }
+    }
+    data.close()
+}
+
+fn non_zero_path_square_svgdata(
+    points: &Vec<(f64, f64)>,
+    clip_pos: f64,
+    scale: f64,
+    square_size: f64,
+) -> SVGData {
+    let mut data = SVGData::new();
+    for j in points {
+        if j.1 != 0.0 {
+            data = data.move_to((
+                scale * (j.1 * (j.0.cos()) + clip_pos + square_size / 2.0),
+                scale * (j.1 * (j.0.sin()) + clip_pos + square_size / 2.0),
+            ));
+            data = data.line_by((scale * -square_size, 0));
+            data = data.line_by((0, scale * -square_size));
+            data = data.line_by((scale * square_size, 0));
+            data = data.close();
+        }
+    }
+    data.close()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
+use ptfl_reader::svg_empty_document;
+use ptfl_reader::svg_output_to_document;
 use ptfl_reader::Config;
 use ptfl_reader::PtflParser;
 use std::env;
-use svg::node::element::path::Data;
-use svg::node::element::Path;
-use svg::node::element::Rectangle;
-use svg::Document;
 
 fn main() {
-    let scale = 250.0;
+    let scale = 1000.0;
     let clip_pos = 2.0;
     let args: Vec<String> = env::args().collect();
     let config = match Config::new(&args) {
@@ -40,109 +38,25 @@ fn main() {
         }
     }
 
+    let mut all_document = svg_empty_document(scale, clip_pos);
+    let mut this_hue = 0.0;
     for i in &point_files {
-        let rect = Rectangle::new()
-            .set("fill", "black")
-            .set("width", "100%")
-            .set("height", "100%");
-
-        let all_path = Path::new()
-            .set("fill", "none")
-            .set("stroke", "navy")
-            .set("stroke-width", scale * 0.0005)
-            .set("d", all_pathdata(&i.1, clip_pos, scale));
-
-        let nz_path = Path::new()
-            .set("fill", "none")
-            .set("stroke", "aqua")
-            .set("stroke-width", scale * 0.002)
-            .set("d", non_zero_pathdata(&i.1, clip_pos, scale));
-
-        let points_path = Path::new()
-            .set("fill", "none")
-            .set("stroke", "yellow")
-            .set("stroke-width", scale * 0.001)
-            .set("d", non_zero_path_square_data(&i.1, clip_pos, scale, 0.006));
-
-        let document = Document::new()
-            .set(
-                "viewBox",
-                (0, 0, scale * clip_pos * 2.0, scale * clip_pos * 2.0),
-            )
-            .add(rect)
-            .add(all_path)
-            .add(nz_path)
-            .add(points_path);
-
+        let document = svg_empty_document(scale, clip_pos);
+        let document = svg_output_to_document(document, &i.1, clip_pos, scale, 0.0, 50);
+        all_document = svg_output_to_document(all_document, &i.1, clip_pos, scale, this_hue, 50);
+        this_hue = if this_hue + 7.0 > 360.0 {
+            this_hue - 353.0
+        } else {
+            this_hue + 7.0
+        };
         svg::save(format!("./tests/{}.svg", i.0), &document).unwrap();
     }
+    svg::save(format!("./tests/all.svg"), &all_document).unwrap();
+    
 }
 
 fn print_help() {
     println!("ptfl_reader [files] [--help]");
     println!("    files: one or multiple file path as input");
     println!("    help: print this message");
-}
-
-fn all_pathdata(points: &Vec<(f64, f64)>, clip_pos: f64, scale: f64) -> Data {
-    let mut data = Data::new().move_to((
-        scale * (&points[0].1 * &points[0].0.cos() + clip_pos),
-        scale * (&points[0].1 * &points[0].0.sin() + clip_pos),
-    ));
-    for j in points {
-        data = data.line_to((
-            scale * (j.1 * j.0.cos() + clip_pos),
-            scale * (j.1 * j.0.sin() + clip_pos),
-        ));
-    }
-    data.close()
-}
-
-fn non_zero_pathdata(points: &Vec<(f64, f64)>, clip_pos: f64, scale: f64) -> Data {
-    let mut data = Data::new().move_to({
-        let mut a = points.iter();
-        loop {
-            let j = match a.next() {
-                Some(next) => next,
-                None => break (0.0, 0.0),
-            };
-            if j.1 != 0.0 {
-                break (
-                    scale * (j.1 * (j.0.cos()) + clip_pos),
-                    scale * (j.1 * (j.0.sin()) + clip_pos),
-                );
-            }
-        }
-    });
-    for j in points {
-        if j.1 != 0.0 {
-            data = data.line_to((
-                scale * (j.1 * (j.0.cos()) + clip_pos),
-                scale * (j.1 * (j.0.sin()) + clip_pos),
-            ));
-        }
-    }
-    data.close()
-}
-
-fn non_zero_path_square_data(
-    points: &Vec<(f64, f64)>,
-    clip_pos: f64,
-    scale: f64,
-    square_size: f64,
-) -> Data {
-    let mut data = Data::new();
-    for j in points {
-        if j.1 != 0.0 {
-            data = data.move_to((
-                scale * (j.1 * (j.0.cos()) + clip_pos + square_size / 2.0),
-                scale * (j.1 * (j.0.sin()) + clip_pos + square_size / 2.0),
-            ));
-            data = data.line_by((scale * -square_size, 0));
-            data = data.line_by((0, scale * -square_size));
-            data = data.line_by((scale * square_size, 0));
-            data = data.close();
-        }
-    }
-    data.close()
 }
