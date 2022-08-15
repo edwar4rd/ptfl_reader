f525574347b0adade77fd71046854e83f866a5d9
edwar4rd <edwar4rd@gmail.com>
2022-08-15 11:44:00 +0800

    Point file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 
 fn main() {
@@ -9,7 +10,7 @@ fn main() {
             print_help();
             println!("\nError happened parsing args:\n\t{err}");
             return;
-        } 
+        }
     };
 
     if config.help {
@@ -17,8 +18,59 @@ fn main() {
         return;
     }
 
-    for i in config.filenames {
-        print!("{i} ");
+    let mut point_files: Vec<(String, Vec<(f64, f64)>)> = Vec::new();
+    for path in config.filenames {
+        let file = match fs::read_to_string(&path) {
+            Ok(file) => file,
+            Err(_) => {
+                println!("Error happened parsing file {path}: ");
+                println!("\tFailed reading file");
+                return;
+            }
+        };
+
+        enum ParsingState {
+            None,
+            DuringReg(usize),
+        }
+
+        let file_entry: u32 = 0;
+        let mut my_state = ParsingState::None;
+        let mut current_reg: Vec<(f64, f64)> = Vec::new();
+        for line in file.lines() {
+            my_state = match my_state {
+                ParsingState::None => {
+                    if line.is_empty() {
+                        ParsingState::None
+                    } else {
+                        let reg_length: usize = match line.parse() {
+                            Ok(length) => length,
+                            Err(_) => {
+                                println!("Error happened parsing file {path}: ");
+                                println!("\tExpected u32 or empty when None");
+                                return;
+                            }
+                        };
+                        current_reg.clear();
+                        current_reg.reserve(reg_length);
+                        ParsingState::DuringReg(reg_length)
+                    }
+                }
+                ParsingState::DuringReg(remaining) => {
+                    let next: usize = remaining - 1;
+                    let reg: Vec<&str> = line.split(',').collect();
+                    let reg: (f64, f64) = (reg[0].trim().parse().expect(""), reg[1].trim().parse().expect(""));
+                    current_reg.push(reg);
+                    if next == 0 {
+                        point_files.push((format!("{path}-{file_entry}"), current_reg.clone()));
+                        ParsingState::None
+                    } else {
+                        ParsingState::DuringReg(remaining - 1)
+                    }
+                }
+            }
+        }
+        println!("Currently {} regs!", point_files.len());
     }
     println!("");
 
