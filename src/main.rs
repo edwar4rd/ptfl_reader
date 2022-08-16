74eda5969868ea31d5c7bbf256852f92e0fd4a44
edwar4rd <edwar4rd@gmail.com>
2022-08-17 01:20:01 +0800

    Fix mismatch with rendered SVG
    use Transform::identity for Transform
    handle scaling manualy
        (f64 as f32 * f32 ->f64 * f64)
    restore opacity previously removed in last commit for debugging

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,8 @@ fn main() {
         let mut points_path_builder = PathBuilder::new();
 
         all_path_builder.move_to(
-            ((&i.1)[0].1 * (&i.1)[0].0.cos() + clip_pos) as f32,
-            ((&i.1)[0].1 * (&i.1)[0].0.sin() + clip_pos) as f32,
+            (scale * ((&i.1)[0].1 * (&i.1)[0].0.cos() + clip_pos)) as f32,
+            (scale * ((&i.1)[0].1 * (&i.1)[0].0.sin() + clip_pos)) as f32,
         );
         let mut entry_iter = i.1.iter();
         if loop {
@@ -61,21 +61,21 @@ fn main() {
                 None => break false,
             };
 
-            let x: f32 = (j.1 * j.0.cos() + clip_pos) as f32;
-            let y: f32 = (j.1 * j.0.sin() + clip_pos) as f32;
+            let x = scale * (j.1 * j.0.cos() + clip_pos);
+            let y = scale * (j.1 * j.0.sin() + clip_pos);
 
             // its possible to both move_to(x, y) and line_to(x, y),
             // but that's not a issue
-            all_path_builder.line_to(x, y);
+            all_path_builder.line_to(x as f32, y as f32);
             if j.1 != 0.0 {
                 // this might never be executed if all point is (angle, 0)
                 // this is handled later by matching .finish()
-                non_zero_path_builder.move_to(x, y);
-                points_path_builder.move_to(x + 0.005, y + 0.005);
-                points_path_builder.line_to(x - 0.005, y + 0.005);
-                points_path_builder.line_to(x - 0.005, y - 0.005);
-                points_path_builder.line_to(x + 0.005, y - 0.005);
-                points_path_builder.line_to(x + 0.005, y + 0.005);
+                non_zero_path_builder.move_to(x as f32, y as f32);
+                points_path_builder.move_to((x + scale * 0.005) as f32, (y + scale * 0.005) as f32);
+                points_path_builder.line_to((x - scale * 0.005) as f32, (y + scale * 0.005) as f32);
+                points_path_builder.line_to((x - scale * 0.005) as f32, (y - scale * 0.005) as f32);
+                points_path_builder.line_to((x + scale * 0.005) as f32, (y - scale * 0.005) as f32);
+                points_path_builder.line_to((x + scale * 0.005) as f32, (y + scale * 0.005) as f32);
                 points_path_builder.close();
                 break true;
             }
@@ -86,16 +86,21 @@ fn main() {
                     None => break,
                 };
 
-                let x: f32 = (j.1 * j.0.cos() + clip_pos) as f32;
-                let y: f32 = (j.1 * j.0.sin() + clip_pos) as f32;
-                all_path_builder.line_to(x, y);
+                let x = scale * (j.1 * j.0.cos() + clip_pos);
+                let y = scale * (j.1 * j.0.sin() + clip_pos);
+                all_path_builder.line_to(x as f32, y as f32);
                 if j.1 != 0.0 {
-                    non_zero_path_builder.line_to(x, y);
-                    points_path_builder.move_to(x + 0.005, y + 0.005);
-                    points_path_builder.line_to(x - 0.005, y + 0.005);
-                    points_path_builder.line_to(x - 0.005, y - 0.005);
-                    points_path_builder.line_to(x + 0.005, y - 0.005);
-                    points_path_builder.line_to(x + 0.005, y + 0.005);
+                    non_zero_path_builder.line_to(x as f32, y as f32);
+                    points_path_builder
+                        .move_to((x + scale * 0.005) as f32, (y + scale * 0.005) as f32);
+                    points_path_builder
+                        .line_to((x - scale * 0.005) as f32, (y + scale * 0.005) as f32);
+                    points_path_builder
+                        .line_to((x - scale * 0.005) as f32, (y - scale * 0.005) as f32);
+                    points_path_builder
+                        .line_to((x + scale * 0.005) as f32, (y - scale * 0.005) as f32);
+                    points_path_builder
+                        .line_to((x + scale * 0.005) as f32, (y + scale * 0.005) as f32);
                     points_path_builder.close();
                 }
             }
@@ -103,7 +108,6 @@ fn main() {
 
         all_path_builder.close();
         non_zero_path_builder.close();
-            
 
         let mut pixmap = Pixmap::new(
             (2.0 * clip_pos * scale) as u32,
@@ -116,51 +120,45 @@ fn main() {
         paint.set_color_rgba8(0, 0, 0, 255);
         pixmap
             .fill_rect(
-                Rect::from_xywh(0.0, 0.0, 2.0 * clip_pos as f32, 2.0 * clip_pos as f32).unwrap(),
+                Rect::from_xywh(
+                    0.0,
+                    0.0,
+                    (scale * 2.0 * clip_pos) as f32,
+                    (scale * 2.0 * clip_pos) as f32,
+                )
+                .unwrap(),
                 &paint,
-                Transform::from_scale(scale as f32, scale as f32),
+                Transform::identity(),
                 None,
             )
             .unwrap();
 
         let rgba = Hsl::new(0.0, 0.4, 0.5).to_rgb();
-        paint.set_color_rgba8(rgba.r, rgba.g, rgba.b, (1.0 * 255.0) as u8);
+        paint.set_color_rgba8(rgba.r, rgba.g, rgba.b, (0.3 * 255.0) as u8);
         let mut stroke = Stroke::default();
-        stroke.width = 0.0005 as f32; // hairline
+        stroke.width = (0.0005 * scale) as f32; // hairline
         pixmap.stroke_path(
             &all_path_builder.finish().unwrap(),
             &paint,
             &stroke,
-            Transform::from_scale(scale as f32, scale as f32),
+            Transform::identity(),
             None,
         );
 
         let rgba = Hsl::new(0.0, 0.7, 0.5).to_rgb();
-        paint.set_color_rgba8(rgba.r, rgba.g, rgba.b, (1.0 * 255.0) as u8);
+        paint.set_color_rgba8(rgba.r, rgba.g, rgba.b, (0.6 * 255.0) as u8);
         let mut stroke = Stroke::default();
-        stroke.width = 0.003 as f32;
+        stroke.width = (0.003 * scale) as f32;
         if let Some(non_zero_path) = non_zero_path_builder.finish() {
-            pixmap.stroke_path(
-                &non_zero_path,
-                &paint,
-                &stroke,
-                Transform::from_scale(scale as f32, scale as f32),
-                None,
-            );
+            pixmap.stroke_path(&non_zero_path, &paint, &stroke, Transform::identity(), None);
         }
 
         let rgba = Hsl::new(0.0, 1.0, 0.5).to_rgb();
-        paint.set_color_rgba8(rgba.r, rgba.g, rgba.b, (1.0 * 255.0 as f64).ceil() as u8);
+        paint.set_color_rgba8(rgba.r, rgba.g, rgba.b, (0.8 * 255.0) as u8);
         let mut stroke = Stroke::default();
-        stroke.width = 0.002 as f32;
+        stroke.width = (0.002 * scale) as f32;
         if let Some(points_path) = points_path_builder.finish() {
-            pixmap.stroke_path(
-                &points_path,
-                &paint,
-                &stroke,
-                Transform::from_scale(scale as f32, scale as f32),
-                None,
-            );
+            pixmap.stroke_path(&points_path, &paint, &stroke, Transform::identity(), None);
         }
 
         pixmap.save_png(format!("./tests/{}.png", i.0)).unwrap();
